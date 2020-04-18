83f6d00565427f1f7fda8a6935da7dd97056ab2f
derekShaheen <38998433+derekShaheen@users.noreply.github.com>
2020-04-17 20:01:33 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -7,26 +7,52 @@ viewer = pannellum.viewer('panorama', {
       "pitch": 2.5,
       "yaw": 13,
       "cssClass": "custom-hotspot",
-      "clickHandlerFunc": function () { handleOpenPanel("bulletin") },
+      "clickHandlerFunc": function () { handleOpenRightPanel("bulletin") },
       "createTooltipFunc": hotspot,
       "createTooltipArgs": "CSS bulletin"
     },
     {
-      "pitch": 5,
-      "yaw": 237,
+      "pitch": -3,
+      "yaw": -119,
       "cssClass": "custom-hotspot",
-      "clickHandlerFunc": function () { handleOpenPanel("amazingPhoto") },
+      "clickHandlerFunc": function () { handleOpenLeftPanel("historicalArchives") },
       "createTooltipFunc": hotspot,
-      "createTooltipArgs": "Historial image"
+      "createTooltipArgs": "Historical Archives"
     },
     {
       "pitch": -26,
       "yaw": 40,
       "cssClass": "custom-hotspot",
-      "clickHandlerFunc": function () { handleOpenPanel("newspaper") },
+      "clickHandlerFunc": function () { handleOpenModal("newspaper") },
       "createTooltipFunc": hotspot,
       "createTooltipArgs": "CSS Newspaper"
+    },
+    // Coffee edit
+    {
+      "pitch": -4,
+      "yaw": 31,
+      "cssClass": "custom-hotspot",
+      "clickHandlerFunc": function () { handleOpenModal("coffeePot") },
+      "createTooltipFunc": hotspot,
+      "createTooltipArgs": "Coffee Pot"
+    },
+    {
+      "pitch": 5,
+      "yaw": 179,
+      "cssClass": "custom-hotspot",
+      "clickHandlerFunc": function () { handleOpenModal("window") },
+      "createTooltipFunc": hotspot,
+      "createTooltipArgs": "Window"
+    },
+    {
+      "pitch": 5,
+      "yaw": 123,
+      "cssClass": "custom-hotspot",
+      "clickHandlerFunc": function () { handleOpenModal("artGallery") },
+      "createTooltipFunc": hotspot,
+      "createTooltipArgs": "Art Gallery"
     }
+
   ],
   // DS 4/12/2020
   // Make it load automatically, and turn on the hotspot debugger.
@@ -44,34 +70,62 @@ viewer = pannellum.viewer('panorama', {
 });
 
 /*begin global constants*/
-var SIDEPANEL_ID = 'sidepanel';
-var SIDEPANEL_CLOSE_ID = 'sidepanelClose';
-var SIDEPANEL_SHOW_CLASS = 'show-sidepanel';
+var RIGHT_SIDEPANEL_ID = 'rightSidepanel';
+var LEFT_SIDEPANEL_ID = 'leftSidepanel';
+var MODAL_ID = 'modal';
+var LEFT_SIDEPANEL_CLOSE_ID = 'leftSidepanelClose';
+var RIGHT_SIDEPANEL_CLOSE_ID = 'rightSidepanelClose';
+var MODAL_CLOSE_ID = 'modalClose';
+var CONTENT_CONTAINER_SHOW_CLASS = 'show-contentContainer';
 var CONTENT_SHOW_CLASS = 'show-content';
 /*end global constants*/
 
-function handleOpenPanel(hotspotContentId) {
-  var sidepanel = document.getElementById(SIDEPANEL_ID);
-  var hotspotContentDiv = document.getElementById(hotspotContentId);
-  var hotspotsToHide = document.getElementsByClassName(CONTENT_SHOW_CLASS);
+function handleOpenLeftPanel(hotspotContentId) {
+  handleOpenContentContainer(hotspotContentId, LEFT_SIDEPANEL_ID);
+}
 
-  sidepanel.classList.remove(SIDEPANEL_SHOW_CLASS);
-  Array.prototype.forEach.call(hotspotsToHide, function (hotspot) {
-    hotspot.classList.remove(CONTENT_SHOW_CLASS)
+function handleOpenRightPanel(hotspotContentId) {
+  handleOpenContentContainer(hotspotContentId, RIGHT_SIDEPANEL_ID);
+}
+
+function handleOpenModal(hotspotContentId) {
+  handleOpenContentContainer(hotspotContentId, MODAL_ID);
+}
+
+function closeAllOpenContentHolders(classThatDeterminesHideShow) {
+  elementsToHide = document.getElementsByClassName(classThatDeterminesHideShow);
+
+  Array.prototype.forEach.call(elementsToHide, function (hotspot) {
+    hotspot.classList.remove(classThatDeterminesHideShow)
   });
+}
+
+function handleOpenContentContainer(hotspotContentId, contentContainerId) {
+  closeAllOpenContentHolders(CONTENT_SHOW_CLASS);
+  closeAllOpenContentHolders(CONTENT_CONTAINER_SHOW_CLASS);
+
+  var contentContainer = document.getElementById(contentContainerId);
+  var hotspotContentDiv = document.getElementById(hotspotContentId);
+
   setTimeout(function () {
-    sidepanel.classList.add(SIDEPANEL_SHOW_CLASS);
+    contentContainer.classList.add(CONTENT_CONTAINER_SHOW_CLASS);
     hotspotContentDiv.classList.add(CONTENT_SHOW_CLASS);
   }, 500);
 }
 
-function addSidepanelActions() {
-  var sidepanel = document.getElementById(SIDEPANEL_ID);
-  var sidepanelClose = document.getElementById(SIDEPANEL_CLOSE_ID);
+function addCloseClickHandlers(contentContainerId, contentContainerCloseId) {
+  var contentContainer = document.getElementById(contentContainerId);
+  var contentContainerClose = document.getElementById(contentContainerCloseId);
 
-  sidepanelClose.addEventListener('click', function (event) {
-    sidepanel.classList.remove(SIDEPANEL_SHOW_CLASS);
+  contentContainerClose.addEventListener('click', function () {
+    contentContainer.classList.remove(CONTENT_CONTAINER_SHOW_CLASS);
   }, false)
+}
+
+function addContentContainerActions() {
+  addCloseClickHandlers(RIGHT_SIDEPANEL_ID, RIGHT_SIDEPANEL_CLOSE_ID)
+  addCloseClickHandlers(LEFT_SIDEPANEL_ID, LEFT_SIDEPANEL_CLOSE_ID)
+  addCloseClickHandlers(MODAL_ID, MODAL_CLOSE_ID)
 }
 
 /*start pannellum setup code*/
@@ -88,5 +142,71 @@ function hotspot(hotSpotDiv, args) {
 
 
 /*start custom function calls*/
-addSidepanelActions();
+addContentContainerActions();
 /*end custom function calls*/
+
+
+//ES, 4-17-20, I tried to create a carousel
+//but I couldn't quite get it to work.
+//I think the issue is in determining the width of the slide in the slides array.
+//When I tried "console.log(slideWidth);" to check if it was working, it returned 0.
+//Maybe this is because it's in a modal window.
+//I followed a tutorial, I wouldn't know how to do this otherwise.
+//I spent a good deal of time on it so I wanted to put it here, but feel free to change this so we have something that works!
+
+/*start art gallery code*/
+/*
+var track = document.querySelector('.carousel_track');
+var slides = Array.from(track.children);
+var nextButton = document.querySelector('#buttonRight');
+var prevButton = document.querySelector('#buttonLeft');
+var dotsNav = document.querySelector('.carousel_nav');
+var dots = Array.from(dotsNav.children);
+
+var slideWidth = slides[0].getBoundingClientRect().width;
+
+console.log(track.children);
+
+//Arrange the slides next to one another
+var setSlidePosition = function(slide, index) {
+  slide.style.left = slideWidth * index + 'px';
+}
+slides.forEach(setSlidePosition);
+
+var moveToSlide = function(track, currentSlide, targetSlide) {
+  track.style.transform = 'translateX(-' + targetSlide.style.left + ')'
+  currentSlide.classList.remove('current-slide');
+  targetSlide.classList.add('current-slide');
+}
+
+//When I click right, move slides to the right
+nextButton.addEventListener('click', e => {
+  var currentSlide = track.querySelector('.current-slide');
+  var nextSlide = currentSlide.nextElementSibling;
+
+  moveToSlide(track, currentSlide, nextSlide);
+})
+
+//When I click left, move slides to the left
+prevButton.addEventListener('click', e => {
+  var currentSlide = track.querySelector('.current-slide');
+  var prevSlide = currentSlide.previousElementSibling;
+
+  moveToSlide(track, currentSlide, prevSlide);
+})
+
+
+//Whn I click nav indicators, move to that slides
+dotsNav.addEventListener('click', e => {
+  // what indicator was clicked on?
+  var targetDot = e.target.closest('button');
+
+  if (!targetDot) return;
+
+  var currentSlide = track.querySelector('.current-slide');
+  var currentDot = dotsNav.querySelector('.current-slide');
+  var targetIndex = dots.findIndex(dot => dot === targetDot);
+  console.log(targetIndex);
+})
+*/
+/*end art gallery code*/
